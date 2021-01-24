406d2fdb6d9b53488f1d26c7f9b6e3f0d3c573d5
Ahira <justiceahira@gmail.com>
2021-01-25 00:37:33 +0100

    Create base entity

---
+++ src/Blog.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Blog.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public long Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
