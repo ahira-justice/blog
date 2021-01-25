2b98caaae9c85a4ab83e75aa6a1f757de3d544bc
Ahira <justiceahira@gmail.com>
2021-01-26 00:33:13 +0100

    Create register dto and validator

---
+++ src/Blog.API/Dtos/Auth/Request/RegisterDto.cs
@@ -0,0 +1,8 @@
+namespace Blog.API.Dtos.Auth.Request
+{
+    public class RegisterDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ src/Blog.API/Validators/Auth/Request/RegisterDtoValidator.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Blog.Application.Repositories.AuthRepo;
+using Blog.API.Dtos.Auth.Request;
+using FluentValidation;
+
+namespace Blog.API.Validators.Auth.Request
+{
+    public class RegisterDtoValidator : NullReferenceAbstractValidator<RegisterDto>
+    {
+        private readonly IAuthRepository _repo;
+        public RegisterDtoValidator(IAuthRepository repo)
+        {
+            _repo = repo;
+
+            RuleFor(dto => dto.Username)
+                .NotEmpty().WithMessage("Username is required")
+                .NotNull().WithMessage("Username is required")
+                .MustAsync(UserDoesNotExist).WithMessage("Username already exists");
+            RuleFor(dto => dto.Password)
+                .NotEmpty().WithMessage("Password is required")
+                .NotNull().WithMessage("Password is required")
+                .Must(IsValidPassword).WithMessage("Password must be at least 8 characters long");
+        }
+
+        private async Task<bool> UserDoesNotExist(string username, CancellationToken cancellationToken)
+        {
+            return username == null ? false : !await _repo.UserExists(username.ToLower());
+        }
+
+        private bool IsValidPassword(string password)
+        {
+            return password == null ? false : password.Length >= 8;
+        }
+    }
+}
