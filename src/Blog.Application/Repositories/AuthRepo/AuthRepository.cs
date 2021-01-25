a48c005bd4f189f9c7112c5c44f63d99fbe55d35
Ahira <justiceahira@gmail.com>
2021-01-25 03:14:19 +0100

    Create auth repository interface and implementation

---
+++ src/Blog.Application/Repositories/AuthRepo/AuthRepository.cs
@@ -0,0 +1,79 @@
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Domain.Entities;
+using Blog.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Application.Repositories.AuthRepo
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public AuthRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> Register(User user, string password)
+        {
+            (var passwordHash, var passwordSalt) = CreatePasswordHash(password);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        public async Task<User> Login(string username, string password)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+
+            if (user == null)
+                return null;
+
+            if (!VerifyPassword(password, user.PasswordHash, user.PasswordSalt))
+                return null;
+
+            return user;
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            if (await _context.Users.AnyAsync(x => x.Username == username))
+                return true;
+
+            return false;
+        }
+
+        private(byte[], byte[]) CreatePasswordHash(string password)
+        {
+            using(var hmac = new HMACSHA512())
+            {
+                var passwordSalt = hmac.Key;
+                var passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                return (passwordHash, passwordSalt);
+            }
+        }
+
+        private bool VerifyPassword(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using(var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i])
+                        return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
