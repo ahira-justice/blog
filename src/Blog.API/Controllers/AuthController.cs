d47ecab34b6341966314501a8d27720460e74c9e
Ahira <justiceahira@gmail.com>
2021-01-26 00:33:24 +0100

    Create user register endpoint

---
+++ src/Blog.API/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using Blog.Application.Repositories.AuthRepo;
+using Blog.API.Dtos.Auth.Request;
+using Blog.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.API.Controllers
+{
+    [Route("api/blog/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [Route("register")]
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterDto request)
+        {
+            var userToCreate = new User { Username = request.Username.ToLower() };
+
+            var createdUser = await _repo.Register(userToCreate, request.Password);
+
+            return StatusCode(201);
+        }
+    }
+}
