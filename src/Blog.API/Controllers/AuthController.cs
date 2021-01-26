a6289e159ac1307099192c8f5e9d5b18a2c6d46a
Ahira <justiceahira@gmail.com>
2021-01-26 23:40:18 +0100

    Expand register endpoint to include user profile details

--- src/Blog.API/Controllers/AuthController.cs
+++ src/Blog.API/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using Blog.Application.Repositories.AuthRepo;
+using Blog.Application.Repositories.UserRepo;
 using Blog.Domain.Dtos.Auth.Request;
 using Blog.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
@@ -10,19 +11,31 @@ namespace Blog.API.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo)
+        private readonly IAuthRepository _authRepo;
+        private readonly IUserRepository _userRepo;
+        public AuthController(IAuthRepository authRepo, IUserRepository userRepo)
         {
-            _repo = repo;
+            _authRepo = authRepo;
+            _userRepo = userRepo;
         }
 
         [Route("register")]
         [HttpPost]
         public async Task<IActionResult> Register([FromBody] RegisterDto request)
         {
-            var userToCreate = new User { Username = request.Username.ToLower() };
+            var userToCreate = new User
+            {
+                Username = request.Username.ToLower()
+            };
+            var createdUser = await _authRepo.Register(userToCreate, request.Password);
 
-            var createdUser = await _repo.Register(userToCreate, request.Password);
+            var profileToCreate = new UserProfile
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                UserId = createdUser.Id
+            };
+            var createdProfile = _userRepo.CreateUser(profileToCreate);
 
             return StatusCode(201);
         }
--- src/Blog.Domain/Dtos/Auth/Request/RegisterDto.cs
+++ src/Blog.Domain/Dtos/Auth/Request/RegisterDto.cs
@@ -4,5 +4,7 @@ namespace Blog.Domain.Dtos.Auth.Request
     {
         public string Username { get; set; }
         public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
