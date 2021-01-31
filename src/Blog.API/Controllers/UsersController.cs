55cc5b6887618102bc5d79e1ffb00ade603861c9
Ahira <justiceahira@gmail.com>
2021-01-31 03:12:49 +0100

    Implement get, getall and update user endpoints

--- src/Blog.API/Controllers/UsersController.cs
+++ src/Blog.API/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using AutoMapper;
 using Blog.Application.Repositories.UserRepo;
 using Blog.Application.Services.UserProfile;
-using Blog.Domain.Dtos.Auth.Request;
 using Blog.Domain.Dtos.Auth.Response;
+using Blog.Domain.Dtos.UserProfile;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -28,6 +30,42 @@ namespace Blog.API.Controllers
         public IActionResult Me()
         {
             var userProfile = _currentUserService.CurrentUser.Profile;
+            var response = _mapper.Map<UserDto>(userProfile);
+            return Ok(response);
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        public async Task<IActionResult> Get([FromRoute] long id)
+        {
+            var userProfile = await _userRepo.GetUserById(id);
+            var response = _mapper.Map<UserDto>(userProfile);
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var userProfiles = await _userRepo.GetUsers();
+            var response = new List<UserDto>();
+
+            foreach (var userProfile in userProfiles)
+            {
+                response.Add(_mapper.Map<UserDto>(userProfile));
+            }
+
+            return Ok(response);
+        }
+
+        [Route("{id}")]
+        [HttpPut]
+        public async Task<IActionResult> Update([FromRoute] long id, [FromBody] UpdateUserDto request)
+        {
+            var user = await _userRepo.GetUserById(id);
+            user.FirstName = request.FirstName;
+            user.LastName = request.LastName;
+
+            var userProfile = await _userRepo.UpdateUser(user);
             var response = _mapper.Map<UserDto>(userProfile);
             return Ok(response);
         }
