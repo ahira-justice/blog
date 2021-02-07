b76285f7d8178c254946d0adf1486ceb37642f68
Ahira <justiceahira@gmail.com>
2021-02-07 18:47:45 +0100

    Override SaveChangesAsync to set CreatedOn & UpdatedOn

--- src/Blog.Persistence/Data/ApplicationDbContext.cs
+++ src/Blog.Persistence/Data/ApplicationDbContext.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Blog.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,5 +18,22 @@ namespace Blog.Persistence.Data
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
         }
 
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedOn = DateTime.UtcNow;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.UpdatedOn = DateTime.UtcNow;
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
