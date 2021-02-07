77111ff6df25f7193c44658f972717ea0af61b04
Ahira <justiceahira@gmail.com>
2021-02-07 18:27:53 +0100

    Add new fields to user entity

--- src/Blog.Domain/Entities/User.cs
+++ src/Blog.Domain/Entities/User.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Blog.Domain.Entities
 {
     public class User : BaseEntity
@@ -5,6 +7,9 @@ namespace Blog.Domain.Entities
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime? LastActive { get; set; }
+        public DateTime? LastLogin { get; set; }
         public UserProfile Profile { get; set; }
     }
 }
---
+++ src/Blog.Persistence/Migrations/20210207172613_AddFieldsToUserEntity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Blog.Persistence.Migrations
+{
+    public partial class AddFieldsToUserEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Users",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastActive",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastLogin",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastActive",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastLogin",
+                table: "Users");
+        }
+    }
+}
